f6601c2d31270f08bea17cf13cd84159a67a64be
Felpscc <126705189+Felpscc@users.noreply.github.com>
2023-08-09 17:13:34 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+import json
+import xmltodict
+import os
+import pandas as pd
+
+def pegar_infos(nome_arquivo,valores):
+    print(f"Informação recebida {nome_arquivo}")
+    with open(f'nfs/{nome_arquivo}',"rb") as arquivo_xml:
+        dic_arquivo = xmltodict.parse(arquivo_xml)
+        print(json.dumps(dic_arquivo, indent=4))
+        if "NFe" in dic_arquivo:
+            infos_nf = dic_arquivo["NFe"]['infNFe']
+            
+        else:
+            infos_nf = dic_arquivo['nfeProc']["NFe"]['infNFe']
+        if "@id" in dic_arquivo:               
+            numero_nota = infos_nf["@id"]
+        else:
+            numero_nota = infos_nf["@Id"]     
+            
+        empresa_emissora = infos_nf['emit']['xNome']
+        nome_cliente = infos_nf["dest"]["xNome"]
+        endereco = infos_nf["dest"]["enderDest"]
+        if "vol" in infos_nf["transp"]:
+            peso = infos_nf["transp"]["vol"]["pesoB"]
+        else:
+            peso = "Não informado"
+        print(numero_nota,empresa_emissora,nome_cliente,endereco,peso, sep="\n")
+        valores.append([numero_nota,empresa_emissora,nome_cliente,endereco,peso])   
+        
+lista_arquivos = os.listdir("nfs")
+
+colunas = ["numero_nota","empresa_emissora","nome_cliente","endereco","peso"]
+valores = []
+
+
+for arquivo in lista_arquivos:
+    pegar_infos(arquivo, valores)
+
+tabela = pd.DataFrame(columns=colunas,data=valores)
+print(tabela)                           
+tabela.to_excel("NotasFiscais.xlsx", index=False)
